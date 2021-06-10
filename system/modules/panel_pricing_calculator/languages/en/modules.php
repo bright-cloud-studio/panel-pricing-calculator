afae3f601812d3a508c94aad57803c4a2f65151b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-10 14:12:26 -0400

    Update modules.php

--- system/modules/panel_pricing_calculator/languages/en/modules.php
+++ system/modules/panel_pricing_calculator/languages/en/modules.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Rep Finder - Location Plugin for Contao
+ * Panel Pricing Calculator - Customized calculator for determining panel pricing
  *
  * Copyright (C) 2021 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/rep-finder
+ * @package    bright-cloud-studio/panel-pricing-calculator
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
