f48796a1e6ab24bd2ee45c0c2864265eceb32581
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-11 14:07:09 -0400

    Update and rename tl_simple_inventory_tracker.php to tl_panel_pricing_calculator.php

---
+++ system/modules/panel_pricing_calculator/languages/en/tl_panel_pricing_calculator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['product_legend'] 	= 'Product Details';
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['publish_legend'] 	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['alias'] 			           = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['product_name'] 			   	= array('Product Name', 'The name of the product');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['product_inventory'] 		= array('Product Inventory', 'The inventory total');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['published'] 		        = array('Published', 'Save this product\'s record.');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
