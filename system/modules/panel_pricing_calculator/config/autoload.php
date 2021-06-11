39e1f758cdff575e441445345dbb83b5599fde99
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-11 13:51:44 -0400

    Add files via upload

---
+++ system/modules/panel_pricing_calculator/config/autoload.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Simple Inventory Tracker - A simple way to track inventory manually.
+ *
+ * Copyright (C) 2021 Mark St. Jean.
+ *
+ * @package    stjeanmark/simple_inventory_tracker
+ * @link       http://www.markstjean.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'Asc\Model\SimpleInventoryTracker' 		=> 'system/modules/simple_inventory_tracker/library/Asc/Model/SimpleInventoryTracker.php'
+));
+
+ClassLoader::addClasses(array
+(
+	'CustomTags\SimpleInventoryTrackerInsertTag' 		=> 'system/modules/simple_inventory_tracker/library/CustomTags/SimpleInventoryTrackerInsertTag.php'
+));
---
+++ system/modules/panel_pricing_calculator/config/config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Simple Inventory Tracker - A simple way to track inventory manually.
+ *
+ * Copyright (C) 2021 Mark St. Jean.
+ *
+ * @package    stjeanmark/simple_inventory_tracker
+ * @link       http://www.markstjean.com
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/**
+* Back end modules
+*/
+
+// DECLARING LOCATIONS BACK END PLUGIN
+$GLOBALS['BE_MOD']['content']['simple_inventory_tracker'] = array(
+	'tables' => array('tl_simple_inventory_tracker'),
+	'icon'   => 'system/modules/simple_inventory_tracker/assets/icons/simple_inventory_tracker.png',
+	'exportLocations' => array('Asc\Backend\SimpleInventoryTrackerBackend', 'exportSimpleInventoryTracker')
+);
+
+/**
+ * Models
+ */
+$GLOBALS['TL_MODELS']['tl_simple_inventory_tracker'] = 'Asc\Model\SimpleInventoryTracker';
+
+
+/**
+ * Hooks
+ */
+if (\Config::getInstance()->isComplete()) {
+	$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('CustomTags\SimpleInventoryTrackerInsertTag', 'onReplaceTag');
+}
