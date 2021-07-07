031a274f62bca581ed3f1f7fba872f2d00b47944
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-07-07 14:19:08 -0400

    Update modules.php

--- system/modules/panel_pricing_calculator/languages/en/modules.php
+++ system/modules/panel_pricing_calculator/languages/en/modules.php
@@ -12,3 +12,4 @@
 
 /* Back end modules */
 $GLOBALS['TL_LANG']['MOD']['panel_pricing_calculator'] = array('Panel Pricing Calculator', 'Track data for panel pricing calculator');
+$GLOBALS['TL_LANG']['MOD']['cradle_depths'] = array('Cradle Depths', 'Add/Edit cradle depths');
