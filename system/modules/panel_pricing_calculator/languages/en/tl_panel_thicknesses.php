8d48e24e7f30d988c7a01c41a4d5b8457471bb1f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-07-07 14:39:51 -0400

    Update and rename tl_panel_thickness.php to tl_panel_thicknesses.php

---
+++ system/modules/panel_pricing_calculator/languages/en/tl_panel_thicknesses.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['product_legend'] 	= 'Product Details';
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['publish_legend'] 	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['label'] 			   	= array('Panel Thickness Label', 'The name of this particular cradle depth');
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['published'] 		        = array('Published', 'Save this product\'s record.');
+ 
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['new']   		 	= array('New panel thickness', 'Add a new panel thickness');
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['show']  		 	= array('Panel Thickness details', 'Show the details of panel thickness ID %s');
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['edit']  		 	= array('Edit panel thickness', 'Edit panel thickness ID %s');
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['copy']  		 	= array('Copy panel thickness', 'Copy panel thickness ID %s');
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['delete'] 			= array('Delete panel thickness', 'Delete panel thickness ID %s');
+$GLOBALS['TL_LANG']['tl_panel_thicknesses']['toggle'] 			= array('Toggle panel thickness', 'Toggle panel thickness ID %s');
