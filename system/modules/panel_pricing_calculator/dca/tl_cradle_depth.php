cde5c234c4edf74725bf648bfc92cf2c91a45e06
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-07-07 14:04:41 -0400

    Create tl_cradle_depth.php

---
+++ system/modules/panel_pricing_calculator/dca/tl_cradle_depth.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+/* Table tl_cradle_depth */
+$GLOBALS['TL_DCA']['tl_cradle_depth'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 0,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('label'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_depth']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_depth']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_depth']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_cradle_depth']['toggle'],
+				'icon'                => 'visible.gif',
+				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+				'button_callback'     => array('Bcs\Backend\CradleDepth', 'toggleIcon')
+			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_depth']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{product_legend},label;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+          (
+            'sql'                     	=> "int(10) unsigned NOT NULL auto_increment"
+          ),
+        'tstamp' => array
+          (
+		        'sql'                     	=> "int(10) unsigned NOT NULL default '0'"
+          ),
+	'sorting' => array
+	(
+		'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
+	),
+	'label' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_depth']['label'],
+		'inputType'               => 'text',
+		'default'		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'published' => array
+	(
+		'exclude'                 => true,
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_depth']['published'],
+		'inputType'               => 'checkbox',
+		'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+		'sql'                     => "char(1) NOT NULL default ''"
+	)		
+    )
+);
