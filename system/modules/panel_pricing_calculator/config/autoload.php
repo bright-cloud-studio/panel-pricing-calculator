973da8b078f909786ee483ad7413050dd28fba4a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-07-09 13:30:28 -0400

    Update autoload.php

--- system/modules/panel_pricing_calculator/config/autoload.php
+++ system/modules/panel_pricing_calculator/config/autoload.php
@@ -16,8 +16,18 @@
  */
 ClassLoader::addClasses(array
 (
+	// front end module
+	'Bcs\Module\PanelPricingCalculatorModule' 	=> 'system/modules/panel_pricing_calculator/library/Bcs/Module/PanelPricingCalculatorModule.php',
+	
+	// different dca to hold data
 	'Bcs\Model\PanelPricingCalculator' 		=> 'system/modules/panel_pricing_calculator/library/Bcs/Model/PanelPricingCalculator.php',
 	'Bcs\Backend\PanelPricingCalculatorBackend' 	=> 'system/modules/panel_pricing_calculator/library/Bcs/Backend/PanelPricingCalculatorBackend.php',
 	'Bcs\Backend\CradleDepthsBackend' 		=> 'system/modules/panel_pricing_calculator/library/Bcs/Backend/CradleDepthsBackend.php',
 	'Bcs\Backend\PanelThicknessesBackend' 		=> 'system/modules/panel_pricing_calculator/library/Bcs/Backend/PanelThicknessesBackend.php'
+));
+
+/* Register the templates */
+TemplateLoader::addFiles(array
+(
+	'mod_panel_pricing_calculator'			=> 'system/modules/panel_pricing_calculator/templates/modules'
 ));
