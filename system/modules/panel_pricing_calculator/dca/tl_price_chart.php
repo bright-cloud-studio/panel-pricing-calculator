b9c54c3996a0e62b67b561039bb7778696db2ac3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-07-28 14:04:31 -0400

    Update and rename tl_panel_pricing_calculator.php to tl_price_chart.php

--- system/modules/panel_pricing_calculator/dca/tl_price_chart.php
+++ system/modules/panel_pricing_calculator/dca/tl_price_chart.php
@@ -11,8 +11,8 @@
  */
 
  
-/* Table tl_panel_pricing_calculator */
-$GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
+/* Table tl_price_chart */
+$GLOBALS['TL_DCA']['tl_price_chart'] = array
 (
  
     // Config
@@ -57,34 +57,34 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
         (
             'edit' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['edit'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart']['edit'],
                 'href'                => 'act=edit',
                 'icon'                => 'edit.gif'
             ),
 			
             'copy' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['copy'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart']['copy'],
                 'href'                => 'act=copy',
                 'icon'                => 'copy.gif'
             ),
             'delete' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['delete'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
                 'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
 			(
-				'label'               => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['toggle'],
+				'label'               => &$GLOBALS['TL_LANG']['tl_price_chart']['toggle'],
 				'icon'                => 'visible.gif',
 				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
-				'button_callback'     => array('Bcs\Backend\PanelPricingCalculatorBackend', 'toggleIcon')
+				'button_callback'     => array('Bcs\Backend\PriceChartBackend', 'toggleIcon')
 			),
             'show' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['show'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart']['show'],
                 'href'                => 'act=show',
                 'icon'                => 'show.gif'
             )
@@ -114,21 +114,21 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'alias' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['alias'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['alias'],
 		'exclude'                 => true,
 		'inputType'               => 'text',
 		'search'                  => true,
 		'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
 		'save_callback' => array
 		(
-			array('Bcs\Backend\PanelPricingCalculatorBackend', 'generateAlias')
+			array('Bcs\Backend\PriceChartBackend', 'generateAlias')
 		),
 		'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
 
 	),
 	'size' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['size'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['size'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -137,7 +137,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'square_feet' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['square_feet'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['square_feet'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -146,7 +146,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'1_8_1' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['1_8_1'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['1_8_1'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -155,7 +155,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'1_8_2' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['1_8_2'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['1_8_2'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -164,7 +164,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'1_8_3' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['1_8_3'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['1_8_3'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -173,7 +173,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'3_4_1' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['3_4_1'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['3_4_1'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -182,7 +182,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	),
 	'3_4_2' => array
 	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['3_4_2'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['3_4_2'],
 		'inputType'               => 'text',
 		'default'		  => '',
 		'search'                  => true,
@@ -192,7 +192,7 @@ $GLOBALS['TL_DCA']['tl_panel_pricing_calculator'] = array
 	'published' => array
 	(
 		'exclude'                 => true,
-		'label'                   => &$GLOBALS['TL_LANG']['tl_panel_pricing_calculator']['published'],
+		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart']['published'],
 		'inputType'               => 'checkbox',
 		'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
 		'sql'                     => "char(1) NOT NULL default ''"
