fa2689232bab2400249eb96cd9483bab0b5e1790
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-07 14:37:28 -0400

    Update PriceChartFunctions.php

--- system/modules/panel_pricing_calculator/library/Bcs/Backend/PriceChartFunctions.php
+++ system/modules/panel_pricing_calculator/library/Bcs/Backend/PriceChartFunctions.php
@@ -1,6 +1,17 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
 
 // This is the main object and itss functions. Everything starts, besides the constructor, at processForm().
-class PanelCalculator extends \Contao\Module
+class PanelCalculator extends \Backend
 {
 	public static $cart_cookie_name = 'ampersandart_cart';
 	public function __construct()
