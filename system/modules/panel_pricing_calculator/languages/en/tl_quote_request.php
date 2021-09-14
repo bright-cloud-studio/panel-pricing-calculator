e593abcff90a5f6654c64829b05c2356620b4602
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-09-14 13:54:01 -0400

    Create tl_quote_request.php

---
+++ system/modules/panel_pricing_calculator/languages/en/tl_quote_request.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_quote_request']['size_legend'] 	= 'Size Details';
+$GLOBALS['TL_LANG']['tl_quote_request']['publish_legend'] 	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_quote_request']['alias'] 			           = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_quote_request']['published'] 		        = array('Published', 'Save this product\'s record.');
+
+/* Sizes */
+$GLOBALS['TL_LANG']['tl_quote_request']['session_id']                    = array('Session ID', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['panel_type']                    = array('Panel Type', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['thickness']                     = array('Thickness', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['cradle']                        = array('Cradle', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['width']                         = array('Width', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['height']                        = array('Height', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['quantity']                      = array('Quantity', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['discount']                      = array('Discount', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_quote_request']['price']                         = array('Price', 'The size of the product');
+
+
+
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_price_chart']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_price_chart']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
