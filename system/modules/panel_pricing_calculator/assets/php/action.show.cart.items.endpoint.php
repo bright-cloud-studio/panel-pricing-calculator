5b784d8a27f62684e252785b196aed375fb207a5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-10-12 19:39:51 -0400

    Update action.show.cart.items.endpoint.php

--- system/modules/panel_pricing_calculator/assets/php/action.show.cart.items.endpoint.php
+++ system/modules/panel_pricing_calculator/assets/php/action.show.cart.items.endpoint.php
@@ -13,13 +13,10 @@
 		//$ourQuote = array("panel_id"=>$vars['panel_id'], "flat_id"=>$vars['flat_id'], "cradle_id"=>$vars['cradle_id'], "width"=>$vars['width'], "height"=>$vars['height'], "quantity"=>$vars['quantity']);
 		
 		$test = $test . '<div id="cart_item_'.$key.'" class="cart_item form-group size100 flex_wrapper">
-			<div class="size20 no-pad"><img src="system/modules/panel_pricing_calculator/assets/images/claybord.jpg"></div>
+			<div class="size20 no-pad"><img style="height: 50px;" src="system/modules/panel_pricing_calculator/assets/images/'.getPanelNameByID($clean['panel_id']).'.jpg"></div>
 			<div class="size30">
-				<div><strong id="cart_name_label">Panel: </strong><span>'.$clean['panel_id'].'</span></div>
-				<div><strong id="cart_thickness_label">Thickness: </strong><span>'.$clean['flat_id'].'</span></div>
-				<div><strong id="cart_cradle_wrapper">Cradle: </strong><span>'.$clean['cradle_id'].'</span></div>
-				<div><strong id="cart_width_label">Width: </strong><span>'.$clean['width'].'</span></div>
-				<div><strong id="cart_height_label">Height: </strong><span>'.$clean['height'].'</span></div>
+				<div><strong id="cart_name_label">Panel: </strong><span>'.$clean['width'].'x'.$clean['height'].' '.getPanelNameByID($clean['panel_id']).', '.getPanelThicknessFromID($clean['flat_id']).'</span></div>
+				<div><strong id="cart_cradle_wrapper">Cradle: </strong><span>'.getPanelCradleFromID($clean['cradle_id']).'</span></div>
 			</div>
 			<div class="size30">
 				<div id="cart_quantity_wrapper"><strong id="quantity_label">Quantity: </strong><span">'.$clean['quantity'].'</span></div>
@@ -33,4 +30,75 @@
 	}
 	
 	echo $test;
+	
+	
+	
+
+// Functions
+//////////////////////////
+
+function getPanelNameByID($panel_id){
+	switch ($panel_id) {
+		case 1:
+			return "Claybord";
+			break;
+		case 2:
+			return "Gessobord";
+			break;
+		case 3:
+			return "Aquabord";
+			break;
+		case 4:
+			return "Encausticbord";
+			break;
+		case 5:
+			return "Scratchbord";
+			break;
+		case 6:
+			return "Pastelbord";
+			break;
+		case 7:
+			return "Hardbord";
+			break;
+	}
+}	
+
+function getPanelThicknessFromID($panel_id){
+	switch ($panel_id) {
+		case 1:
+			return '1/8"';
+			break;
+		case 2:
+			return '1/4"';
+			break;
+	}
+}
+
+function getPanelCradleFromID($panel_id){
+	switch ($panel_id) {
+		case "none":
+			return "None";
+			break;
+		case "3":
+			return "3/4\" Cradled";
+			break;
+		case "4":
+			return "1\" Cradled";
+			break;
+		case "5":
+			return "1 1/2\" Cradled";
+			break;
+		case "6":
+			return "2\" Cradled";
+			break;
+		case "7":
+			return "2 1/2\" Cradled";
+			break;
+		case "8":
+			return "3\" Cradled";
+			break;
+	}
+}
+
+	
 ?>
