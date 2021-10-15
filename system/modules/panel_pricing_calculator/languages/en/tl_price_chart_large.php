2651ff18f5222d44cb364b65323385145aed9b75
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-10-15 13:31:53 -0400

    Create tl_price_chart_large.php

---
+++ system/modules/panel_pricing_calculator/languages/en/tl_price_chart_large.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_price_chart_large']['size_legend'] 	= 'Size Details';
+$GLOBALS['TL_LANG']['tl_price_chart_large']['products_legend'] 	= 'Products Details';
+$GLOBALS['TL_LANG']['tl_price_chart_large']['publish_legend'] 	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_price_chart_large']['alias'] 			           = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['published'] 		        = array('Published', 'Save this product\'s record.');
+
+/* Sizes */
+$GLOBALS['TL_LANG']['tl_price_chart_large']['size'] 			     	      = array('Size', 'The size of the product');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['width'] 			     	      = array('Width', 'The width of the product');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['height'] 			     	      = array('Height', 'The height of the product');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['square_feet']         = array('Square Feet', 'The square feet of the product');
+
+/* Products */
+$GLOBALS['TL_LANG']['tl_price_chart_large']['1_8_1']         = array('1/8" FLAT Claybord, Gessobord, Aquabord, Encausticbord', ' ');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['1_8_2']         = array('1/8" FLAT Scratchbord, Pastelbord', ' ');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['1_8_3']         = array('1/8" FLAT Hardbord', ' ');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['3_4_1']         = array('3/4" cradled Hardbord', ' ');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['3_4_2']         = array('3/4" cradled Other', ' ');
+
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_price_chart_large']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_price_chart_large']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
