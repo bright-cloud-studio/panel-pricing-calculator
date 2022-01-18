bf4c0ddbcf14c4bd4de5d5f75623dfc562ab6599
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-01-17 21:49:30 -0500

    Create tl_cradle_prices.php

---
+++ system/modules/panel_pricing_calculator/dca/tl_cradle_prices.php
@@ -0,0 +1,183 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+ 
+/* Table tl_cradle_prices */
+$GLOBALS['TL_DCA']['tl_cradle_prices'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'alias' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => 0,
+            'panelLayout'             => 'filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('width'),
+            'format'                  => '<span style="font-weight: bold;">Width:</span> %s'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_prices']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_prices']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_prices']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_cradle_prices']['toggle'],
+				'icon'                => 'visible.gif',
+				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+				'button_callback'     => array('Bcs\Backend\CradlePricesBackend', 'toggleIcon')
+			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cradle_prices']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{price_legend},1_inch,1_5_inch,2_inch,2_5_inch,3_inch;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+		'sql'                     	=> "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+		'sql'                     	=> "int(10) unsigned NOT NULL default '0'"
+        ),
+	'sorting' => array
+	(
+		'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
+	),
+	'alias' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['alias'],
+		'exclude'                 => true,
+		'inputType'               => 'text',
+		'search'                  => true,
+		'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+		'save_callback' => array
+		(
+			array('Bcs\Backend\CradlePricesBackend', 'generateAlias')
+		),
+		'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+
+	),
+	'1_inch' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['1_inch'],
+		'inputType'               => 'text',
+		'default'		  		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'1_5_inch' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['1_5_inch'],
+		'inputType'               => 'text',
+		'default'		  		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'2_inch' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['2_inch'],
+		'inputType'               => 'text',
+		'default'		  		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'2_5_inch' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['2_5_inch'],
+		'inputType'               => 'text',
+		'default'		  		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'3_inch' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['3_inch'],
+		'inputType'               => 'text',
+		'default'		  		  => '',
+		'search'                  => true,
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+		'sql'                     => "varchar(255) NOT NULL default ''"
+	),
+	'published' => array
+	(
+		'exclude'                 => true,
+		'label'                   => &$GLOBALS['TL_LANG']['tl_cradle_prices']['published'],
+		'inputType'               => 'checkbox',
+		'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+		'sql'                     => "char(1) NOT NULL default ''"
+	)		
+    )
+);
