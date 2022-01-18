3ad8f36e93fd93f3fabebdafa7627ce6b55d0b14
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-01-17 21:51:42 -0500

    Create tl_cradle_prices.php

---
+++ system/modules/panel_pricing_calculator/languages/en/tl_cradle_prices.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Panel Pricing Calculator - Adds a panel pricing calculator with a front end module for users and a back end system for managing the data.
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/panel-pricing-calculator
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_cradle_prices']['price_legend'] 	= 'Price Details';
+$GLOBALS['TL_LANG']['tl_cradle_prices']['publish_legend'] 	= 'Publish';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_cradle_prices']['alias'] 			        = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['published'] 		        = array('Published', 'Save this product\'s record.');
+
+/* Cradle Sizes and Prices */
+$GLOBALS['TL_LANG']['tl_cradle_prices']['1_inch'] 		= array('1" Cradle Price', 'The price per square inch');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['1_5_inch']     = array('1.5" Cradle Price', 'The price per square inch');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['2_inch']       = array('2" Cradle Price', 'The price per square inch');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['2_5_inch']     = array('2.5" Cradle Price', 'The price per square inch');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['3_inch']       = array('3" Cradle Price', 'The price per square inch');
+
+ /* Buttons */
+$GLOBALS['TL_LANG']['tl_cradle_prices']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_cradle_prices']['toggle'] 			= array('Toggle record', 'Toggle record ID %s');
