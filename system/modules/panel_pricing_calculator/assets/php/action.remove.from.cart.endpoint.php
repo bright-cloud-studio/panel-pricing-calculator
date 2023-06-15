a1e757581755cebc0882babf0aa13e8a2a1f69ac
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-15 10:21:06 -0400

    Update action.remove.from.cart.endpoint.php

--- system/modules/panel_pricing_calculator/assets/php/action.remove.from.cart.endpoint.php
+++ system/modules/panel_pricing_calculator/assets/php/action.remove.from.cart.endpoint.php
@@ -1,14 +1,11 @@
 <?php
-// Starts the session and connects to the database
-include_once("prepend.cart.endpoint.php");
+
+    // Starts the session and connects to the database
+    include_once("prepend.cart.endpoint.php");
 
 	// turn the form values into $vars
 	$vars = $_POST;
 	
-	
 	unset($_SESSION['asdf'][$vars['entry_id']]);
-	
 	$$_SESSION['asdf'] = array_values($$_SESSION['asdf']);
-		
 	echo count($_SESSION['asdf']);
-		
