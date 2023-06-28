0c9530311fa40f649ea39778adbe29a0d891a1a9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-28 13:27:37 -0400

    Update action.remove.from.cart.endpoint.php

--- system/modules/panel_pricing_calculator/assets/php/action.remove.from.cart.endpoint.php
+++ system/modules/panel_pricing_calculator/assets/php/action.remove.from.cart.endpoint.php
@@ -6,6 +6,9 @@
 	// turn the form values into $vars
 	$vars = $_POST;
 	
+	// Unset removes the entry in the $_SESSION['asdf'] array at the index of the entry_id
 	unset($_SESSION['asdf'][$vars['entry_id']]);
+	// idk what this line is doing anymore so I wont be removing it
 	$_SESSION['asdf'] = array_values($_SESSION['asdf']);
+	// echo out the total number of entries in our session array
 	echo count($_SESSION['asdf']);
