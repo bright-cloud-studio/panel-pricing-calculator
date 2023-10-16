8c9eeeef27bda97c1e7fd33fa454c934f610cdb3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-16 12:43:38 -0400

    Update action.send.email.php

--- system/modules/panel_pricing_calculator/assets/php/action.send.email.php
+++ system/modules/panel_pricing_calculator/assets/php/action.send.email.php
@@ -47,7 +47,7 @@ include_once("prepend.cart.endpoint.php");
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 	// More headers
-	$headers .= 'From: <bords@ampersandart.com>' . "\r\n";
+	$headers .= 'From: <orders@ampersandart.com>' . "\r\n";
 	$headers .= 'Cc: <web@brightcloudstudio.com>, <bords@ampersandart.com>' . "\r\n";
 	
 
