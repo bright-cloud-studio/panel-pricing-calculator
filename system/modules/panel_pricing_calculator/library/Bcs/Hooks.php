2948070eaff61420c61bfc96364d68f8158f1daf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-01-30 11:14:53 -0500

    Update Hooks.php

--- system/modules/panel_pricing_calculator/library/Bcs/Hooks.php
+++ system/modules/panel_pricing_calculator/library/Bcs/Hooks.php
@@ -15,6 +15,7 @@ use Contao\Database;
 
 use Isotope\Interfaces\IsotopeProduct;
 use Isotope\Isotope;
+use Isotope\Model\Attribute;
 use Isotope\Model\AttributeOption;
 
 use Isotope\Model\Product;
@@ -28,53 +29,179 @@ class Hooks
         
         // If this is our "Add to cart" button
         if($formData['formID'] == 'calc_add_to_cart') {
-            
-            /** Dev Stuffs
-            echo "HOOK: Successful<br><br>";
-            
-            echo "<pre>";
-            print_r($submittedData);
-            echo "</pre>";
-            
-            echo "<pre>";
-            print_r($formData);
-            echo "</pre>";
-            die();
-            */
-            
-            
-            // STEP ONE
-            // Build our product query by getting the proper IDs for the selected options
-            
-            $parent_id = 1466;
-            
-            $custom_width = AttributeOption::findOneBy(['tl_iso_attribute_option.label=?'],[$submittedData['width'] . "&#34;"])->id;
-            $custom_height = AttributeOption::findOneBy(['tl_iso_attribute_option.label=?'],[$submittedData['height'] . "&#34;"])->id;
 
-            $custom_depth = 230;
-            $custom_thickness = 236;
+            // STEP ONE
+            // First we will gather all of our IDs based on the submitted form so that we can query Isotope and see if this product exists or not
             
+            $parent_id = $this->getProductTypeID($submittedData['panel_id']);
+            $attribute_id_custom_width = Attribute::findOneBy(['tl_iso_attribute.field_name=?'],["custom_width"])->id;
+            $attribute_id_custom_height = Attribute::findOneBy(['tl_iso_attribute.field_name=?'],["custom_height"])->id;
+            $attribute_id_custom_depth = Attribute::findOneBy(['tl_iso_attribute.field_name=?'],["custom_depth"])->id;
+            $attribute_id_custom_thickness = Attribute::findOneBy(['tl_iso_attribute.field_name=?'],["custom_thickness"])->id;
+            $custom_width = AttributeOption::findOneBy(['tl_iso_attribute_option.pid=?', 'tl_iso_attribute_option.label=?'],[$attribute_id_custom_width, $submittedData['width'] . "&#34;"])->id;
+            $custom_height = AttributeOption::findOneBy(['tl_iso_attribute_option.pid=?', 'tl_iso_attribute_option.label=?'],[$attribute_id_custom_height, $submittedData['height'] . "&#34;"])->id;
+            $custom_depth = AttributeOption::findOneBy(['tl_iso_attribute_option.pid=?', 'tl_iso_attribute_option.label=?'],[$attribute_id_custom_depth, $this->getCradleDepthByID($submittedData['cradle_id'])])->id;
+            $custom_thickness = AttributeOption::findOneBy(['tl_iso_attribute_option.pid=?', 'tl_iso_attribute_option.label=?'],[$attribute_id_custom_thickness, $this->getPanelThicknessByID($submittedData['flat_id'])])->id;
             $quantity = $submittedData['quantity'];
 
-            // See if the product exists
+
+            // STEP TWO
+            // Using our collected IDs we will see if this product exists or not
             $prod = Product::findOneBy(['tl_iso_product.pid=?', 'tl_iso_product.custom_width=?', 'tl_iso_product.custom_height=?', 'tl_iso_product.custom_depth=?', 'tl_iso_product.custom_thickness=?'],[$parent_id, $custom_width, $custom_height, $custom_depth, $custom_thickness]);
             if($prod == null) {
                 
-                // Product does not exist, lets create it
-                echo "NOPE";
+                // This product does not exist! Let's create it.
+                
+                
+                //If $custom_width is null, make the attribute
+                if(!$custom_width) {
+                    $attr = new AttributeOption();
+                    $attr->pid = $attribute_id_custom_width;
+                    $attr->sorting = 1000 * $submittedData['width'];
+                    $attr->tstamp = time();
+                    $attr->ptable = 'tl_iso_attribute';
+                    $attr->type = 'option';
+                    $attr->label = $submittedData['width'] . "&#34;";
+                    $attr->published = 1;
+                    $attr->save();
+                    
+                    $custom_width = $attr->id;
+                }
+                
+                if(!$custom_height) {
+                    $attr = new AttributeOption();
+                    $attr->pid = $attribute_id_custom_height;
+                    $attr->sorting = 1000 * $submittedData['height'];
+                    $attr->tstamp = time();
+                    $attr->ptable = 'tl_iso_attribute';
+                    $attr->type = 'option';
+                    $attr->label = $submittedData['height'] . "&#34;";
+                    $attr->published = 1;
+                    $attr->save();
+                    
+                    $custom_height = $attr->id;
+                }
+
+
+                
+                
+                
+                //Of $custom_height is null, make the attribute
+                
+                
+                // Create a variant with the attribute IDs we created
+                
+                // Add the new variant to the cart!
+                
+                
+                
+                
+                
+                echo "custom_width: " . $custom_width . "<br>";
+                echo "custom_height: " . $custom_height . "<br>";
+                echo "custom_depth: " . $custom_depth . "<br>";
+                echo "custom_thickness: " . $custom_thickness . "<br>";
                 die();
                 
             } else {
                 
-                // Product exists, add it to the cart
+                // This product exists! Add it to the cart with the quantity we need
                 $arrConfig = array();
                 if (Isotope::getCart()->addProduct($prod, $quantity, $arrConfig) !== false)
 					$blnAdded = true;
-                
             }
-                
+
         }
         
     }
-  
+    
+    
+    // Pass in the ID from the form's "Panel name" select and get back the ID of that product type
+    private function getProductTypeID($select_id) {
+        switch ($select_id) {
+            // Claybord
+            case 1:
+                return 1466;
+                break;
+            // Gessobord
+            case 2:
+                return -1;
+                break;
+            // Aquabord
+            case 3:
+                return -1;
+                break;
+            // Encausticbord
+            case 4:
+                return -1;
+                break;
+            // Scratchbord
+            case 5:
+                return -1;
+                break;
+            // Pastelbord
+            case 6:
+                return -1;
+                break;
+            // Hardbord
+            case 7:
+                return -1;
+                break;
+            default:
+                return -1;
+        }
+    }
+    
+    // Take the ID and get back the isotope ID we need here
+    private function getCradleDepthByID($select_id) {
+        switch ($select_id) {
+            // Claybord
+            case "none":
+                return "None";
+                break;
+            // Gessobord
+            case 3:
+                return "3/4&#34; Cradled";
+                break;
+            // Aquabord
+            case 4:
+                return "1&#34; Cradled";
+                break;
+            // Encausticbord
+            case 5:
+                return "1 1/2&#34; Cradled";
+                break;
+            // Scratchbord
+            case 6:
+                return "2&#34; Cradled";
+                break;
+            // Pastelbord
+            case 7:
+                return "2 1/2&#34; Cradled";
+                break;
+            // Hardbord
+            case 8:
+                return "3&#34; Cradled";
+                break;
+            default:
+                return -1;
+        }
+    }
+    
+    // Take the ID and get back the isotope ID we need here
+    private function getPanelThicknessByID($select_id) {
+        switch ($select_id) {
+            // 1/8"
+            case 1:
+                return "1/8&#34;";
+                break;
+            // 1/4"
+            case 2:
+                return "1/4&#34;";
+                break;
+            default:
+                return -1;
+        }
+    }
+    
 }
